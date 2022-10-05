7ce85de5304a4e607f0a3b9f62658db372a6ad6a
boa50 <bruno.albuquerque50@gmail.com>
2022-10-05 14:51:03 -0300

    Refactored some code to make it reusable

--- analysis.R
+++ analysis.R
@@ -4,12 +4,12 @@ library(tidyr)
 library(scales)
 library(ggplot2)
 
-theme_boa <- function() {
+theme_boa <- function(title_hjust = -0.14) {
   light_grey <- "#9e9e9e"
   grey <- "#616161"
   
   theme_classic() +
-    theme(plot.title = element_text(hjust = -0.14, colour = grey),
+    theme(plot.title = element_text(hjust = title_hjust, colour = grey),
           axis.line = element_line(colour = light_grey),
           axis.ticks = element_line(colour = light_grey),
           axis.text = element_text(colour = light_grey),
@@ -28,21 +28,20 @@ df_wellbeing <- df %>%
 
 x_labels <- min(df_wellbeing$household_income) * 2^c(0:5)
 
-plot_lines <- function(dataset, x = "log_household_income", 
-                       scale_x_param = scale_x_continuous()) {
-  ggplot(dataset, aes(x = !!sym(x), y = value, color = metric)) +
-    geom_line() +
-    scale_x_param +
-    scale_y_continuous(name = "Score") +
-    scale_color_manual(labels = c("Experienced Well-Being", "Life Satisfaction"),
-                       values = c("#90caf9", "#b0bec5")) +
-    theme_boa() +
-    theme(legend.position = "none",
-          plot.margin = unit(c(.5,6.5,.5,.5), "lines")) +
-    coord_cartesian(clip = "off")
-}
+wellbeing <- list(
+  metric = "experienced_wellbeing_zscore",
+  label = "Experienced Well-Being",
+  color = "#90caf9",
+  label_hjust = .15
+)
 
-### Trying to replicate the chart from the paper
+life_satisfaction <- list(
+  metric = "life_satisfaction_zscore",
+  label = "Life Satisfaction",
+  color = "#b0bec5",
+  label_hjust = .21
+)
+
 get_last_score <- function(metric_name) {
   df_wellbeing %>% 
     filter(metric == metric_name) %>% 
@@ -50,30 +49,39 @@ get_last_score <- function(metric_name) {
     pull(value)
 }
 
+annotate_line <- function(obj) {
+  annotate("text", label = obj$label, hjust = obj$label_hjust,
+           size = 3, color = obj$color, fontface = "bold",
+           x = Inf, y = get_last_score(obj$metric))
+}
+
+plot_lines <- function(dataset, x = "log_household_income", 
+                       scale_x_param = scale_x_continuous()) {
+  ggplot(dataset, aes(x = !!sym(x), y = value, color = metric)) +
+    geom_line() +
+    scale_x_param +
+    labs(title = "Respondents per household income",
+         y = "Score", x = "Household Income") +
+    scale_color_manual(values = c(wellbeing$color, life_satisfaction$color)) +
+    theme_boa(title_hjust = -0.20) +
+    theme(legend.position = "none",
+          plot.margin = unit(c(.5,6.5,.5,.5), "lines")) +
+    coord_cartesian(clip = "off") +
+    annotate_line(wellbeing) +
+    annotate_line(life_satisfaction)
+}
+
+### Trying to replicate the chart from the paper
 plot_lines(df_wellbeing, x = "log_household_income", 
-           scale_x_param = scale_x_continuous(name = "Income", 
-                                              breaks = log(x_labels), 
-                                              labels = dollar(x_labels))) +
-  annotate("text", label = "Experienced Well-Being", hjust = .15,
-           size = 3, color = "#90caf9", fontface = "bold",
-           x = Inf, y = get_last_score("experienced_wellbeing_zscore")) +
-  annotate("text", label = "Life Satisfaction", hjust = .21,
-           size = 3, color = "#b0bec5", fontface = "bold",
-           x = Inf, y = get_last_score("life_satisfaction_zscore"))
+           scale_x_param = scale_x_continuous(breaks = log(x_labels), 
+                                              labels = dollar(x_labels)))
 
 ### Define a fixed width to the chart when saving the image
 
 ### Adjusting the scale
 plot_lines(df_wellbeing, x = "household_income", 
-           scale_x_param = scale_x_continuous(name = "Income", 
-                                              breaks = x_labels[c(1,4:6)], 
-                                              labels = dollar(x_labels[c(1,4:6)]))) +
-  annotate("text", label = "Experienced Well-Being", hjust = .15,
-           size = 3, color = "#90caf9", fontface = "bold",
-           x = Inf, y = get_last_score("experienced_wellbeing_zscore")) +
-  annotate("text", label = "Life Satisfaction", hjust = .21,
-           size = 3, color = "#b0bec5", fontface = "bold",
-           x = Inf, y = get_last_score("life_satisfaction_zscore"))
+           scale_x_param = scale_x_continuous(breaks = x_labels[c(1,4:6)], 
+                                              labels = dollar(x_labels[c(1,4:6)])))
 
 
 ### Plot distribution of respondents per money income
@@ -88,7 +96,7 @@ df_personcount %>%
   geom_col() + 
   labs(title = "Respondents per household income",
        y = "Respondents", x = "Household Income") +
-  scale_y_continuous(expand = expansion(mult = c(0, .1))) +
+  scale_y_continuous(expand = expansion(mult = c(0, .08))) +
   theme_boa() +
   theme(axis.ticks.x = element_blank(),
         axis.line.x = element_blank())
