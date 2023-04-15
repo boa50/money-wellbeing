577d248fcbbc092df00cd06ba8b26fb893d12da7
boa50 <bruno.albuquerque50@gmail.com>
2023-04-16 09:03:32 +0930

    Made some changes in the data preparation process to make the project more reusable

--- analysis.R
+++ analysis.R
@@ -30,15 +30,7 @@ theme_boa <- function() {
           axis.title = element_text(colour = my_colors$axis))
 }
 
-df <- read_csv("datasets/income_wellbeing.csv")
-
-df_wellbeing <- df %>%
-  select(household_income, log_household_income, 
-         experienced_wellbeing_zscore.Mean,
-         life_satisfaction_zscore.Mean) %>% 
-  rename_with(~ gsub(".Mean", "", .)) %>% 
-  pivot_longer(-c(household_income, log_household_income),
-               values_to = "value", names_to = "metric")
+df_wellbeing <- readRDS("data/df_wellbeing.RDS")
 
 x_labels <- min(df_wellbeing$household_income) * 2^c(0:5)
 
@@ -118,11 +110,7 @@ lines_adjusted +
 save_plot("lines_scaled_w_points")
 
 ### Plot distribution of respondents per money income
-df_personcount <- df %>% 
-  select("household_income", ends_with(".personcount")) %>% 
-  mutate(person_count = (select(df, ends_with(".personcount")) %>% do.call(pmax, .)),
-         household_income_k = paste("$", floor(household_income / 1e3), " k", sep = "")) %>% 
-  select(-ends_with(".personcount"))
+df_personcount <- readRDS("data/df_personcount.RDS")
 
 get_person_count <- function(income) {
   df_personcount %>% 
@@ -156,58 +144,7 @@ df_personcount %>%
 save_plot("personcount_histogram")
 
 ### Comparison between GDP and happiness
-df_gdp <- read_csv("datasets/WDIData.csv") %>% 
-  clean_names() %>% 
-  select(c(country_name, indicator_code, x2018:x2020)) %>%
-  # Getting only the "GDP per capita, PPP (constant 2017 international $)" indicator
-  filter(indicator_code == "NY.GDP.PCAP.PP.KD") %>% 
-  # Filtering of the Country groups
-  slice(50:n()) %>% 
-  # Getting rows with at least 1 not null value
-  filter(rowSums(is.na(.)) < 3) %>% 
-  # Getting only the last not NA value
-  mutate(gdp = do.call(coalesce, rev(across(x2018:x2020)))) %>% 
-  select(c(country_name, gdp))
-
-df_happiness <- read_csv2("datasets/happiness_scores.csv") %>% 
-  clean_names() %>% 
-  select(c(country, happiness_score)) %>% 
-  # Remove some characters indicating that the column value was not the last
-  mutate(country = gsub("*", "", .$country, fixed = TRUE))
-
-# Checking names that don't match
-# There is no corresponding value on df_gdp for Taiwan
-df_happiness %>% 
-  merge(df_gdp, by.x = "country", by.y = "country_name", all.x = TRUE) %>% 
-  filter(is.na(gdp))
-
-# Replacing names that are different
-df_gdp$country_name <- df_gdp$country_name %>% 
-  gsub("Congo, Rep.", "Congo", ., fixed = TRUE) %>% 
-  gsub("Egypt, Arab Rep.", "Egypt", ., fixed = TRUE) %>% 
-  gsub("Gambia, The", "Gambia", ., fixed = TRUE) %>% 
-  gsub("Hong Kong SAR, China", "Hong Kong", ., fixed = TRUE) %>% 
-  gsub("Iran, Islamic Rep.", "Iran", ., fixed = TRUE) %>% 
-  gsub("Kyrgyz Republic", "Kyrgyzstan", ., fixed = TRUE) %>% 
-  gsub("Lao PDR", "Laos", ., fixed = TRUE) %>% 
-  gsub("West Bank and Gaza", "Palestine", ., fixed = TRUE) %>% 
-  gsub("Russian Federation", "Russia", ., fixed = TRUE) %>% 
-  gsub("Slovak Republic", "Slovakia", ., fixed = TRUE) %>% 
-  gsub("Korea, Rep.", "South Korea", ., fixed = TRUE) %>% 
-  gsub("Turkiye", "Turkey", ., fixed = TRUE) %>% 
-  gsub("Venezuela, RB", "Venezuela", ., fixed = TRUE) %>% 
-  gsub("Yemen, Rep.", "Yemen", ., fixed = TRUE)
-
-df_happiness$country <- df_happiness$country %>% 
-  gsub("Czechia", "Czech Republic", ., fixed = TRUE) %>% 
-  gsub("Eswatini, Kingdom of", "Eswatini", ., fixed = TRUE) %>% 
-  gsub("Hong Kong S.A.R. of China", "Hong Kong", ., fixed = TRUE) %>% 
-  gsub("Ivory Coast", "Cote d'Ivoire", ., fixed = TRUE) %>% 
-  gsub("North Cyprus", "Cyprus", ., fixed = TRUE) %>% 
-  gsub("Palestinian Territories", "Palestine", ., fixed = TRUE)
-
-df_happiness_gdp <- df_happiness %>% 
-  merge(df_gdp, by.x = "country", by.y = "country_name")
+df_happiness_gdp <- readRDS("data/df_happiness_gdp.RDS")
 
 happiness_median <- median(df_happiness_gdp$happiness_score)
 gdp_median <- median(df_happiness_gdp$gdp)
---
+++ datasets/data_preparation.R
@@ -0,0 +1,82 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+library(janitor)
+
+df <- read_csv("datasets/income_wellbeing.csv")
+
+df_wellbeing <- df %>%
+  select(household_income, log_household_income, 
+         experienced_wellbeing_zscore.Mean,
+         life_satisfaction_zscore.Mean) %>% 
+  rename_with(~ gsub(".Mean", "", .)) %>% 
+  pivot_longer(-c(household_income, log_household_income),
+               values_to = "value", names_to = "metric")
+
+saveRDS(df_wellbeing, "data/df_wellbeing.RDS")
+
+df_personcount <- df %>% 
+  select("household_income", ends_with(".personcount")) %>% 
+  mutate(person_count = (select(df, ends_with(".personcount")) %>% do.call(pmax, .)),
+         household_income_k = paste("$", floor(household_income / 1e3), " k", sep = "")) %>% 
+  select(-ends_with(".personcount"))
+
+saveRDS(df_personcount, "data/df_personcount.RDS")
+
+### Comparison between GDP and happiness
+df_gdp <- read_csv("datasets/WDIData.csv") %>% 
+  clean_names() %>% 
+  select(c(country_name, indicator_code, x2018:x2020)) %>%
+  # Getting only the "GDP per capita, PPP (constant 2017 international $)" indicator
+  filter(indicator_code == "NY.GDP.PCAP.PP.KD") %>% 
+  # Filtering of the Country groups
+  slice(50:n()) %>% 
+  # Getting rows with at least 1 not null value
+  filter(rowSums(is.na(.)) < 3) %>% 
+  # Getting only the last not NA value
+  mutate(gdp = do.call(coalesce, rev(across(x2018:x2020)))) %>% 
+  select(c(country_name, gdp))
+
+df_happiness <- read_csv2("datasets/happiness_scores.csv") %>% 
+  clean_names() %>% 
+  select(c(country, happiness_score)) %>% 
+  # Remove some characters indicating that the column value was not the last
+  mutate(country = gsub("*", "", .$country, fixed = TRUE))
+
+# Checking names that don't match
+# There is no corresponding value on df_gdp for Taiwan
+df_happiness %>% 
+  merge(df_gdp, by.x = "country", by.y = "country_name", all.x = TRUE) %>% 
+  filter(is.na(gdp))
+
+# Replacing names that are different
+df_gdp$country_name <- df_gdp$country_name %>% 
+  gsub("Congo, Rep.", "Congo", ., fixed = TRUE) %>% 
+  gsub("Egypt, Arab Rep.", "Egypt", ., fixed = TRUE) %>% 
+  gsub("Gambia, The", "Gambia", ., fixed = TRUE) %>% 
+  gsub("Hong Kong SAR, China", "Hong Kong", ., fixed = TRUE) %>% 
+  gsub("Iran, Islamic Rep.", "Iran", ., fixed = TRUE) %>% 
+  gsub("Kyrgyz Republic", "Kyrgyzstan", ., fixed = TRUE) %>% 
+  gsub("Lao PDR", "Laos", ., fixed = TRUE) %>% 
+  gsub("West Bank and Gaza", "Palestine", ., fixed = TRUE) %>% 
+  gsub("Russian Federation", "Russia", ., fixed = TRUE) %>% 
+  gsub("Slovak Republic", "Slovakia", ., fixed = TRUE) %>% 
+  gsub("Korea, Rep.", "South Korea", ., fixed = TRUE) %>% 
+  gsub("Turkiye", "Turkey", ., fixed = TRUE) %>% 
+  gsub("Venezuela, RB", "Venezuela", ., fixed = TRUE) %>% 
+  gsub("Yemen, Rep.", "Yemen", ., fixed = TRUE)
+
+df_happiness$country <- df_happiness$country %>% 
+  gsub("Czechia", "Czech Republic", ., fixed = TRUE) %>% 
+  gsub("Eswatini, Kingdom of", "Eswatini", ., fixed = TRUE) %>% 
+  gsub("Hong Kong S.A.R. of China", "Hong Kong", ., fixed = TRUE) %>% 
+  gsub("Ivory Coast", "Cote d'Ivoire", ., fixed = TRUE) %>% 
+  gsub("North Cyprus", "Cyprus", ., fixed = TRUE) %>% 
+  gsub("Palestinian Territories", "Palestine", ., fixed = TRUE)
+
+df_happiness_gdp <- df_happiness %>% 
+  merge(df_gdp, by.x = "country", by.y = "country_name")
+
+saveRDS(df_happiness_gdp, "data/df_happiness_gdp.RDS")
+
+rm(df, df_wellbeing, df_personcount, df_happiness_gdp, df_happiness, df_gdp)
